ec55fc5e3d60bfbe6a4e0f26657985dff63c7356
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-02-12 15:44:12 -0300

    criando a database

--- app/src/main/java/com/devspace/taskbeats/TaskListAdapter.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskListAdapter.kt
@@ -8,40 +8,36 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskListAdapter :
-    ListAdapter<TaskUiData, TaskListAdapter.TaskViewHolder>(TaskListAdapter) {
+class TaskListAdapter : ListAdapter<TaskUiData, TaskListAdapter.TaskViewHolder>(TaskListAdapter) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
         return TaskViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: TaskListAdapter.TaskViewHolder, position: Int) {
         val category = getItem(position)
         holder.bind(category)
     }
 
-    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val tvCategory = view.findViewById<TextView>(R.id.tv_category_name)
+    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val tvCategory=view.findViewById<TextView>(R.id.tv_category_name)
         private val tvTask = view.findViewById<TextView>(R.id.tv_task_name)
 
-        fun bind(task: TaskUiData) {
+        fun bind(task: TaskUiData){
             tvCategory.text = task.category
             tvTask.text = task.name
         }
     }
 
-    companion object : DiffUtil.ItemCallback<TaskUiData>() {
+    companion object: DiffUtil.ItemCallback<TaskUiData>(){
         override fun areItemsTheSame(oldItem: TaskUiData, newItem: TaskUiData): Boolean {
-            return oldItem == newItem
+           return oldItem == newItem
         }
 
         override fun areContentsTheSame(oldItem: TaskUiData, newItem: TaskUiData): Boolean {
-            return oldItem.name == newItem.name
+           return oldItem.name == newItem.name
         }
 
     }
-
-
 }
